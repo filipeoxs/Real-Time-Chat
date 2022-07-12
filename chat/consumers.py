cb62b613131e2638710e2c482bcc680342d8b7fd
filipeoxs <filipeoxs@gmail.com>
2022-07-12 16:50:46 -0300

    Finalizando configuração

--- chat/consumers.py
+++ chat/consumers.py
@@ -14,3 +14,7 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self,text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        self.send(text_data=json.dumps({
+            'type':'chat',
+            'message':message
+        }))
